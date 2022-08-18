ed403b75afe6d3ef9dd60090a7139566e90da8f3
Fabio Lima <fabio.lima.henrique@gmail.com>
2022-08-18 04:31:07 -0300

    implementação de testes de integração

---
+++ src/test/java/com/fabiolima/parking/ParkingControllerIT.java
@@ -0,0 +1,54 @@
+package com.fabiolima.parking;
+
+import com.fabiolima.parking.model.dto.ParkingCreateDTO;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Matches;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ParkingControllerIT {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest() {
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    public void whenFindAllCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/parking")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("license[0]", Matchers.equalTo("YYY-1111"));
+    }
+
+    @Test
+    public void whenCreateThenCheckCreated() {
+
+        ParkingCreateDTO parkingCreateDTO = new ParkingCreateDTO();
+        parkingCreateDTO.setLicense("KWT-0122");
+        parkingCreateDTO.setColor("PRETA");
+        parkingCreateDTO.setState("RN");
+        parkingCreateDTO.setModel("ONIX-LTS");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(parkingCreateDTO)
+                .post("/parking")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license", Matchers.equalTo("KWT-0122"));
+    }
+
+}
